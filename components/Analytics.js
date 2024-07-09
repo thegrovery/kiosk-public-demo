d5715e322ad47a52f1b4070f5b4d7c5ab87d6164
Alec Reimel <alecreimel1@gmail.com>
2024-07-08 21:15:38 -0400

    changed event name var

--- components/Analytics.js
+++ components/Analytics.js
@@ -15,55 +15,55 @@ export default function Analytics({
     console.log("Analytics.js file working!!!");
 
     /* ===== EVENT FUNCTIONS ===== */
-    function ClickAnalyticsEvent(targetElement){
-      //Set vars
-      var evCat = targetElement.attr('data-category') ? targetElement.attr('data-category') : '';
-      var evAct = targetElement.attr('data-action') ? targetElement.attr('data-action') : '';
-      var evLab = targetElement.attr('data-label')  ? targetElement.attr('data-label') : '';
-      var evVal = targetElement.attr('data-value')  ? targetElement.attr('data-value') : '';
+      function ClickAnalyticsEvent(targetElement){
+        //Set vars
+        var evCat = targetElement.attr('data-category') ? targetElement.attr('data-category') : '';
+        var evAct = targetElement.attr('data-action') ? targetElement.attr('data-action') : '';
+        var evLab = targetElement.attr('data-label')  ? targetElement.attr('data-label') : '';
+        var evVal = targetElement.attr('data-value')  ? targetElement.attr('data-value') : '';
 
-      try {
-       //Fire event
-       window.dataLayer = window.dataLayer || [];
-       dataLayer.push({
-         'event':          'GAEvent',
-         'eventCategory':  evCat,
-         'eventAction':    evAct,
-         'eventLabel':     evLab,
-         'eventValue':     evVal,
-       });
+        try {
+         //Fire event
+         window.dataLayer = window.dataLayer || [];
+         dataLayer.push({
+           'event':          'GAEvent',
+           'eventCategory':  evCat,
+           'eventAction':    evAct,
+           'eventLabel':     evLab,
+           'eventValue':     evVal,
+         });
 
-       console.log("GA Event fired - Event Category: ["+evCat+"], Event Label: ["+evLab+"], Event Action: ["+evAct+"]");
+         console.log("GA Event fired - Event Category: ["+evCat+"], Event Label: ["+evLab+"], Event Action: ["+evAct+"]");
 
-      } catch (e) {
-       console.log("GA Event Error");
-      }
-    };
+        } catch (e) {
+         console.log("GA Event Error");
+        }
+      };
 
-    function TestAnalyticsEvent(targetElement){
-      //Set vars
-      let eventName = "custom_event_test";
-      let eventCat = "eventCat";
-      let eventAct = "eventAct";
-      let eventLab = "eventLab";
-      let eventVal = "eventVal";
+      function TestAnalyticsEvent(targetElement){
+        //Set vars
+        let eventName = "GAEvent";
+        let eventCat = "eventCat";
+        let eventAct = "eventAct";
+        let eventLab = "eventLab";
+        let eventVal = "eventVal";
 
-      try {
-       //Fire event
-       window.dataLayer = window.dataLayer || [];
-       dataLayer.push({
-         'event':          eventName,
-         'eventCategory':  eventCat,
-         'eventAction':    eventAct,
-         'eventLabel':     eventLab,
-         'eventValue':     eventVal,
-       });
+        try {
+         //Fire event
+         window.dataLayer = window.dataLayer || [];
+         dataLayer.push({
+           'event':          eventName,
+           'eventCategory':  eventCat,
+           'eventAction':    eventAct,
+           'eventLabel':     eventLab,
+           'eventValue':     eventVal,
+         });
 
-       console.log("GA Event fired - Event Name: ["+eventName+"], Event Category: ["+eventCat+"], Event Label: ["+eventLab+"], Event Action: ["+eventAct+"]");
+         console.log("GA Event fired - Event Name: ["+eventName+"], Event Category: ["+eventCat+"], Event Label: ["+eventLab+"], Event Action: ["+eventAct+"]");
 
-      } catch (e) {
-       console.log("GA Event Error");
-      }
+        } catch (e) {
+         console.log("GA Event Error");
+        }
     };
 
 
